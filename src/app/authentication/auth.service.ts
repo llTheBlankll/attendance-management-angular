e84c43451a770d3e0fabb22072b0c5430a775d1c
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-06-08 12:31:59 +0800

    These changes appear to be related to refactoring and renaming a login component in an Angular application. Here's a summary of the changes:
        Renaming of files and references:
            login.component.ts has been deleted and replaced with auth.component.ts.
            login.component.html has been renamed to auth.component.html.
            login.component.spec.ts has been renamed to auth.component.spec.ts.
            References to LoginComponent have been changed to AuthComponent.
            authentication.ts has been deleted.
        AuthComponent:
            The AuthComponent class has been created in auth.component.ts.
            It includes a login form with form controls for username and password.
            The loginForm has been updated to include validation for minimum and maximum lengths.
            The login method handles authentication by sending a login request to the server.
            If the login is successful, it saves the user information to session storage and redirects to the dashboard.
            If the login fails, it displays an error message.
        AuthService:
            The AuthService class has been created in auth.service.ts.
            It provides methods to authenticate users and check if they are authenticated.
            The isAuthenticated method checks if the user is authenticated by making an API call.
            The login method sends a login request to the server and returns an observable.
        Testing:
            auth.service.spec.ts has been added for testing the AuthService.
            auth.component.spec.ts has been updated to test the AuthComponent.
    
    Overall, these changes seem to be focused on improving the authentication functionality of the application by refactoring the login component and adding additional validation and error handling.

---
+++ src/app/authentication/auth.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {map, Observable, of} from "rxjs";
+import {LoginDTO, ResponseMessage, ResponseMessageV2, ResponseStatus} from "../DTOList";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly loginUrl: string = environment.authenticationUrl;
+  private readonly baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  isAuthenticated(): boolean {
+    let token: string | null = sessionStorage.getItem("token");
+    let username: string | null = sessionStorage.getItem("username");
+
+    if (token && username) {
+      this.http.post<HttpResponse<ResponseMessageV2>>(this.baseUrl + `/api/auth/is-valid?token=${token}&username=${username}`, {responseType: 'json', observe: 'response'})
+        .pipe(
+          map((response: HttpResponse<ResponseMessageV2>) => {
+            let message = response.body;
+
+            if (message?.status == ResponseStatus.VALID) {
+              return of(true);
+            }
+
+            return of(false);
+          })
+        ).subscribe();
+
+      return true;
+    }
+
+    return false;
+  }
+
+  login(login: LoginDTO): Observable<any> {
+    console.log("Requesting login to " + this.loginUrl);
+    return this.http.post<LoginDTO | ResponseMessage>(this.loginUrl, login, {responseType: 'json', observe: 'response'});
+  }
+}
---
+++ src/app/authentication/login/auth.component.ts
@@ -0,0 +1,91 @@
+import {Component, OnInit} from '@angular/core';
+import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatError, MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {MatTooltip} from "@angular/material/tooltip";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {LoginDTO, LoginToken} from '../../DTOList';
+import {catchError, map, of} from "rxjs";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatGridList,
+    MatSlideToggle,
+    MatGridTile,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+    MatCardActions,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatButton,
+    MatCardHeader,
+    MatHint,
+    MatTooltip,
+    ReactiveFormsModule,
+    MatError
+  ],
+  templateUrl: './auth.component.html',
+  styleUrl: './auth.component.css'
+})
+export class AuthComponent {
+
+  loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.minLength(3), Validators.maxLength(32), Validators.required]),
+    password: new FormControl('', [Validators.minLength(5), Validators.maxLength(128), Validators.required])
+  })
+
+  constructor(private loginService: AuthService, private alert: MatSnackBar, private router: Router) {
+    this.loginService = loginService;
+  }
+
+  invalidCredentials(): void {
+    this.alert.open("Username or Password is Incorrect", "Close");
+  }
+
+  login(): void {
+    // Do authentication
+    let loginData: LoginDTO = this.loginForm.value;
+    if (loginData.username.length < 32) {
+      console.log("Logging in");
+      // Check if status code 200
+      this.loginService.login(loginData).pipe(
+        map((response: HttpResponse<any>) => {
+          console.log("Login successful");
+          // Get token
+          let loginToken: LoginToken = response.body;
+          // Save user info to session storage
+          sessionStorage.setItem("token", loginToken.token);
+          sessionStorage.setItem("username", loginToken.username);
+          sessionStorage.setItem("role", loginToken.role);
+          sessionStorage.setItem("expiration", loginToken.expiration);
+
+          // Show alert message
+          this.alert.open("Login successful", "Close");
+
+          // Redirect to home page
+          this.router.navigate(["/dashboard"]).then(_ => console.log("Redirected to dashboard"));
+        }),
+        catchError((error: HttpErrorResponse) => {
+          if (error.status === 400) {
+            this.invalidCredentials();
+          }
+          return of(null);
+        })
+      ).subscribe();
+    } else {
+      // Show error message
+      this.alert.open("Invalid credentials", "Close");
+    }
+  }
+}
