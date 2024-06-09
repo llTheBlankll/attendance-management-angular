50af187e95d1ecdc99b99b9afc3c0f1ac7e96709
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-06-09 13:32:44 +0800

    Environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiUrl: "http://localhost:8080",
+  production: false,
+  authenticationUrl: "http://localhost:8080/api/auth/login"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiUrl: "http://localhost:8080/api",
+  authenticationUrl: "http://localhost:8080/api/auth/login"
+};
